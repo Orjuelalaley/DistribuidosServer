89671da242b68d35ac42d91a0d81dc57c6cdd345
Sebastián Orjuela <sebasorjuela@gmail.com>
2023-08-15 15:42:43 -0500

    Fix in Client some code duplication.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,3 @@
-//cliente
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -12,18 +11,37 @@ public class Client {
         String serverAddress = "localhost";
         int serverPort = 12345;
         int backupServerPort = 12356;
+        boolean connected = false;
         Scanner scanner = new Scanner(System.in);
+        while (!connected) {
+            try {
+                connected = communicateWithServer(serverAddress, serverPort, scanner);
+            } catch (IOException e) {
+                System.out.println("No se pudo conectar al servidor principal. Intentando con el servidor de respaldo...");
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception ex) {
+                    ex.getMessage();
+                }
+                try {
+                    connected = communicateWithServer(serverAddress, backupServerPort, scanner);
+                } catch (IOException a) {
+                    System.out.println("No se pudo conectar al servidor de respaldo. Verifique la conexión.");
+                }
+            }
+        }
+    }
 
-        try {
-            Socket socket = new Socket(serverAddress, serverPort);
-            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+    private static boolean communicateWithServer(String serverAddress, int serverPort, Scanner scanner) throws IOException {
+        try (Socket socket = new Socket(serverAddress, serverPort);
+             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream())) {
 
             List<Producto> productos = new ArrayList<>();
 
             System.out.print("Ingrese la cantidad de productos: ");
             int cantidadProductos = scanner.nextInt();
-            scanner.nextLine(); // Limpiar el buffer
+            scanner.nextLine();
 
             for (int i = 0; i < cantidadProductos; i++) {
                 System.out.print("Ingrese el nombre del producto " + (i + 1) + ": ");
@@ -34,63 +52,17 @@ public class Client {
 
                 System.out.print("Ingrese el precio del producto " + (i + 1) + ": ");
                 double precio = scanner.nextDouble();
-                scanner.nextLine(); // Limpiar el buffer
+                scanner.nextLine();
 
                 Producto producto = new Producto(nombre, categoria, precio);
                 productos.add(producto);
             }
-
             outputStream.writeObject(productos);
-            outputStream.flush(); // Importante: asegúrate de vaciar el búfer
-
+            outputStream.flush();
             double sumaCalculada = inputStream.readDouble();
             System.out.println("Total a pagar: " + sumaCalculada);
 
-            socket.close();
-        } catch (IOException e) {
-            System.out.println("No se pudo conectar al servidor principal. Intentando con el servidor de respaldo...");
-
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-            try {
-                Socket backupSocket = new Socket(serverAddress, backupServerPort);
-                ObjectOutputStream backupOutputStream = new ObjectOutputStream(backupSocket.getOutputStream());
-                ObjectInputStream backupInputStream = new ObjectInputStream(backupSocket.getInputStream());
-
-                List<Producto> productos = new ArrayList<>();
-
-                System.out.print("Ingrese la cantidad de productos: ");
-                int cantidadProductos = scanner.nextInt();
-                scanner.nextLine();
-
-                for (int i = 0; i < cantidadProductos; i++) {
-                    System.out.print("Ingrese el nombre del producto " + (i + 1) + ": ");
-                    String nombre = scanner.nextLine();
-
-                    System.out.print("Ingrese la categoría del producto " + (i + 1) + ": ");
-                    String categoria = scanner.nextLine();
-
-                    System.out.print("Ingrese el precio del producto " + (i + 1) + ": ");
-                    double precio = scanner.nextDouble();
-                    scanner.nextLine();
-
-                    Producto producto = new Producto(nombre, categoria, precio);
-                    productos.add(producto);
-                }
-
-                backupOutputStream.writeObject(productos);
-                backupOutputStream.flush();
-
-                double sumaCalculada = backupInputStream.readDouble();
-                System.out.println("Total a pagar (respaldo): " + sumaCalculada);
-
-                backupSocket.close();
-            } catch (IOException a) {
-                System.out.println("No se pudo conectar al servidor de respaldo. Verifique la conexión.");
-            }
+            return true; // Indica que la comunicación fue exitosa
         }
     }
 }
